bd5fbec9d3d561e4af25debdd40afb34a072f8c3
cenuno <24255819+cenuno@users.noreply.github.com>
2022-03-20 22:24:35 -0700

    Add venv setup instructions

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# create new virtual environment named venv
+python3 -m venv venv
+
+# activate the virtual environment named venv
+source "./venv/bin/activate"
+
+
+# install the same exact packages that I used which are stored in a .txt file
+pip3 install -r requirements.txt
